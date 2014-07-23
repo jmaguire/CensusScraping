93c07dfb0baa788acd4181ee58cfaad41a648c2e
jmaguire <jmaguire@stanford.edu>
2014-07-22 22:31:46 -0700

    works

--- GenerateUSCensusData.py
+++ GenerateUSCensusData.py
@@ -15,16 +15,28 @@ stateURLs = tools.getJSONData(stateFilename)
 '''
 
 data = {}
+try: 
+    data = tools.getJSONData('us_census')
+    print 'loaded'
+except:
+    data = {}
+
+
 for subdir, dirs, files in os.walk('./json'):
     try:
         state = subdir.split('/')[-1]
         cities = tools.getJSONData(subdir + '/cities')
+        print state
         for city in cities:
-            print city,state
+            key = city + '|' + state
+            if key in data:
+                continue
+            else:
+                print 'need to add'
             soup = tools.getSoup(cities[city]['url'])
             try:
-                data[city] = cities[city]
-                data[city][state] = state 
+                data[key] = cities[city]
+                data[key]['state'] = state 
                 for table in soup.findAll('table'):
                     if table.attrs.has_key('title') and 'QuickFacts' in table.attrs['title']:
                         for row in table.findAll('tr'):
@@ -37,15 +49,13 @@ for subdir, dirs, files in os.walk('./json'):
                                     try:
                                         value = tools.getFloat(tds[2].contents[0])
                                     except:
-                                        value = tds[2].contents[0]
-                                
-                                data[city][field] = value
-                break
+                                        value = tds[2].contents[0]                                
+                                data[key][field] = value
             except:
-                break
-                           
+                print 'fatal'
+            tools.saveJSONData(data, 'us_census')                
     except:
         continue
-    break
+    
 
 tools.saveJSONData(data, 'us_census')
